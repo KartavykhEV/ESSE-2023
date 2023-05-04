ba1459051c7020a277fbf15874a0c3376aea93cf
Евгений Картавых <evgeniy@kartavykh.com>
2023-05-04 09:12:13 +0300

    -

--- SendKeys/Form1.Designer.cs
+++ SendKeys/Form1.Designer.cs
@@ -37,13 +37,18 @@ namespace SendKeys
             this.txtWindowHandle = new System.Windows.Forms.TextBox();
             this.lblActiveWindowHandle = new System.Windows.Forms.Label();
             this.grpKeys = new System.Windows.Forms.GroupBox();
-            this.txtText = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
             this.rbtALT = new System.Windows.Forms.RadioButton();
             this.rbtCTRL = new System.Windows.Forms.RadioButton();
             this.cboLetter = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.statusStrip1.SuspendLayout();
             this.grpKeys.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -51,98 +56,90 @@ namespace SendKeys
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.lblCurrentlyActiveWindow});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 773);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 625);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(3, 0, 37, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(1133, 54);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 28, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1424, 42);
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // lblCurrentlyActiveWindow
             // 
             this.lblCurrentlyActiveWindow.Name = "lblCurrentlyActiveWindow";
-            this.lblCurrentlyActiveWindow.Size = new System.Drawing.Size(223, 41);
+            this.lblCurrentlyActiveWindow.Size = new System.Drawing.Size(178, 32);
             this.lblCurrentlyActiveWindow.Text = "Active Window:";
             // 
             // btnLock
             // 
-            this.btnLock.Location = new System.Drawing.Point(813, 29);
-            this.btnLock.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnLock.Location = new System.Drawing.Point(992, 10);
+            this.btnLock.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnLock.Name = "btnLock";
-            this.btnLock.Size = new System.Drawing.Size(293, 55);
+            this.btnLock.Size = new System.Drawing.Size(220, 44);
             this.btnLock.TabIndex = 1;
             this.btnLock.Text = "Lock Window";
             this.btnLock.UseVisualStyleBackColor = true;
+            this.btnLock.Visible = false;
             this.btnLock.Click += new System.EventHandler(this.btnLock_Click);
             // 
             // lblActiveWindow
             // 
             this.lblActiveWindow.AutoSize = true;
-            this.lblActiveWindow.Location = new System.Drawing.Point(35, 41);
-            this.lblActiveWindow.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblActiveWindow.Location = new System.Drawing.Point(32, 17);
+            this.lblActiveWindow.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblActiveWindow.Name = "lblActiveWindow";
-            this.lblActiveWindow.Size = new System.Drawing.Size(209, 32);
+            this.lblActiveWindow.Size = new System.Drawing.Size(159, 25);
             this.lblActiveWindow.TabIndex = 2;
             this.lblActiveWindow.Text = "Active Window:";
             // 
             // txtActiveWindow
             // 
-            this.txtActiveWindow.Location = new System.Drawing.Point(269, 33);
-            this.txtActiveWindow.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtActiveWindow.Enabled = false;
+            this.txtActiveWindow.Location = new System.Drawing.Point(203, 14);
+            this.txtActiveWindow.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtActiveWindow.Name = "txtActiveWindow";
-            this.txtActiveWindow.Size = new System.Drawing.Size(521, 38);
+            this.txtActiveWindow.Size = new System.Drawing.Size(392, 31);
             this.txtActiveWindow.TabIndex = 3;
+            this.txtActiveWindow.Text = "Редактирование обучающегося";
             // 
             // txtWindowHandle
             // 
-            this.txtWindowHandle.Location = new System.Drawing.Point(269, 95);
-            this.txtWindowHandle.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtWindowHandle.Enabled = false;
+            this.txtWindowHandle.Location = new System.Drawing.Point(789, 17);
+            this.txtWindowHandle.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtWindowHandle.Name = "txtWindowHandle";
-            this.txtWindowHandle.Size = new System.Drawing.Size(185, 38);
+            this.txtWindowHandle.Size = new System.Drawing.Size(140, 31);
             this.txtWindowHandle.TabIndex = 4;
             // 
             // lblActiveWindowHandle
             // 
             this.lblActiveWindowHandle.AutoSize = true;
-            this.lblActiveWindowHandle.Location = new System.Drawing.Point(24, 103);
-            this.lblActiveWindowHandle.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblActiveWindowHandle.Location = new System.Drawing.Point(607, 17);
+            this.lblActiveWindowHandle.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblActiveWindowHandle.Name = "lblActiveWindowHandle";
-            this.lblActiveWindowHandle.Size = new System.Drawing.Size(222, 32);
+            this.lblActiveWindowHandle.Size = new System.Drawing.Size(168, 25);
             this.lblActiveWindowHandle.TabIndex = 5;
             this.lblActiveWindowHandle.Text = "Window Handle:";
             // 
             // grpKeys
             // 
-            this.grpKeys.Controls.Add(this.txtText);
-            this.grpKeys.Controls.Add(this.btnSend);
-            this.grpKeys.Controls.Add(this.rbtALT);
-            this.grpKeys.Controls.Add(this.rbtCTRL);
-            this.grpKeys.Controls.Add(this.cboLetter);
-            this.grpKeys.Location = new System.Drawing.Point(32, 157);
-            this.grpKeys.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.grpKeys.Controls.Add(this.richTextBox1);
+            this.grpKeys.Controls.Add(this.panel2);
+            this.grpKeys.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpKeys.Location = new System.Drawing.Point(0, 60);
+            this.grpKeys.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.grpKeys.Name = "grpKeys";
-            this.grpKeys.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.grpKeys.Size = new System.Drawing.Size(1075, 610);
+            this.grpKeys.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.grpKeys.Size = new System.Drawing.Size(1424, 565);
             this.grpKeys.TabIndex = 6;
             this.grpKeys.TabStop = false;
             this.grpKeys.Text = "Send Keys";
             // 
-            // txtText
-            // 
-            this.txtText.Location = new System.Drawing.Point(64, 141);
-            this.txtText.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.txtText.Multiline = true;
-            this.txtText.Name = "txtText";
-            this.txtText.Size = new System.Drawing.Size(701, 449);
-            this.txtText.TabIndex = 6;
-            this.txtText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtText_KeyPress);
-            // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(565, 72);
-            this.btnSend.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnSend.Location = new System.Drawing.Point(391, 40);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(200, 55);
+            this.btnSend.Size = new System.Drawing.Size(150, 41);
             this.btnSend.TabIndex = 5;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -151,10 +148,10 @@ namespace SendKeys
             // rbtALT
             // 
             this.rbtALT.AutoSize = true;
-            this.rbtALT.Location = new System.Drawing.Point(221, 79);
-            this.rbtALT.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.rbtALT.Location = new System.Drawing.Point(138, 48);
+            this.rbtALT.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.rbtALT.Name = "rbtALT";
-            this.rbtALT.Size = new System.Drawing.Size(104, 36);
+            this.rbtALT.Size = new System.Drawing.Size(82, 29);
             this.rbtALT.TabIndex = 4;
             this.rbtALT.TabStop = true;
             this.rbtALT.Text = "ALT";
@@ -163,10 +160,10 @@ namespace SendKeys
             // rbtCTRL
             // 
             this.rbtCTRL.AutoSize = true;
-            this.rbtCTRL.Location = new System.Drawing.Point(64, 79);
-            this.rbtCTRL.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.rbtCTRL.Location = new System.Drawing.Point(20, 48);
+            this.rbtCTRL.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.rbtCTRL.Name = "rbtCTRL";
-            this.rbtCTRL.Size = new System.Drawing.Size(125, 36);
+            this.rbtCTRL.Size = new System.Drawing.Size(98, 29);
             this.rbtCTRL.TabIndex = 3;
             this.rbtCTRL.TabStop = true;
             this.rbtCTRL.Text = "CTRL";
@@ -180,31 +177,82 @@ namespace SendKeys
             "O",
             "P",
             "F4"});
-            this.cboLetter.Location = new System.Drawing.Point(357, 76);
-            this.cboLetter.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.cboLetter.Location = new System.Drawing.Point(235, 40);
+            this.cboLetter.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.cboLetter.Name = "cboLetter";
-            this.cboLetter.Size = new System.Drawing.Size(185, 39);
+            this.cboLetter.Size = new System.Drawing.Size(140, 33);
             this.cboLetter.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(760, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(644, 93);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Отправить строку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(6, 144);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1412, 415);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.lblActiveWindow);
+            this.panel1.Controls.Add(this.btnLock);
+            this.panel1.Controls.Add(this.lblActiveWindowHandle);
+            this.panel1.Controls.Add(this.txtActiveWindow);
+            this.panel1.Controls.Add(this.txtWindowHandle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1424, 60);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.rbtCTRL);
+            this.panel2.Controls.Add(this.cboLetter);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.rbtALT);
+            this.panel2.Controls.Add(this.btnSend);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(6, 30);
+            this.panel2.Margin = new System.Windows.Forms.Padding(6);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(5);
+            this.panel2.Size = new System.Drawing.Size(1412, 114);
+            this.panel2.TabIndex = 9;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1133, 827);
+            this.ClientSize = new System.Drawing.Size(1424, 667);
             this.Controls.Add(this.grpKeys);
-            this.Controls.Add(this.lblActiveWindowHandle);
-            this.Controls.Add(this.txtWindowHandle);
-            this.Controls.Add(this.txtActiveWindow);
-            this.Controls.Add(this.lblActiveWindow);
-            this.Controls.Add(this.btnLock);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.statusStrip1);
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.grpKeys.ResumeLayout(false);
             this.grpKeys.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -224,7 +272,10 @@ namespace SendKeys
         private System.Windows.Forms.RadioButton rbtCTRL;
         private System.Windows.Forms.RadioButton rbtALT;
         private System.Windows.Forms.Button btnSend;
-        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- SendKeys/Form1.cs
+++ SendKeys/Form1.cs
@@ -2,6 +2,9 @@
 using SendKeys.BLL.ActiveWindow;
 using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SendKeys
@@ -21,8 +24,8 @@ namespace SendKeys
 
         private void ActiveWindowWatcher_ActiveWindowChanged(object sender, ActiveWindowChangedEventArgs e)
         {
-            activeWindow = ActiveWindowModel.Create(e.WindowHandle, e.WindowTitle);
-            lblCurrentlyActiveWindow.Text = $"Active Window: {e.WindowTitle}";
+            //activeWindow = ActiveWindowModel.Create(e.WindowHandle, e.WindowTitle);
+            //lblCurrentlyActiveWindow.Text = $"Active Window: {e.WindowTitle}";
         }
 
         private void btnLock_Click(object sender, EventArgs e)
@@ -37,10 +40,55 @@ namespace SendKeys
             WindowAPI.SendKeys(activeWindow.WindowHandle, rbtCTRL.Checked, rbtALT.Checked, cboLetter.Text);
         }
 
+
+
+
+
         private void txtText_KeyPress(object sender, KeyPressEventArgs e)
         {
             WindowAPI.SendKeys(activeWindow.WindowHandle, e.KeyChar.ToString());
             WindowAPI.SetActiveWindow(Process.GetCurrentProcess().MainWindowHandle);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        public IntPtr GetHandleWindow(string title)
+        {
+            return FindWindow(null, title);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (richTextBox1.SelectionStart < richTextBox1.Text.Length)
+            {
+
+                IntPtr handle = GetHandleWindow(txtActiveWindow.Text);
+                txtWindowHandle.Text = handle != null ? handle.ToString() : "0";
+                if (handle == null)
+                    return;
+                activeWindow = ActiveWindowModel.Create(handle, txtActiveWindow.Text);
+
+
+                int cursorPosition = richTextBox1.SelectionStart;
+                int lineIndex = richTextBox1.GetLineFromCharIndex(cursorPosition);
+                string lineText = richTextBox1.Lines[lineIndex];
+                richTextBox1.SelectionStart += lineText.Length + 1;
+                lineText = lineText.Replace(' ', '\t');
+
+                //MessageBox.Show(lineText);
+                foreach (char c in lineText)
+                    WindowAPI.SendKeys(handle, c.ToString());
+                WindowAPI.SendKeys(handle, "\t");
+                WindowAPI.SendKeys(handle, " ");
+                Thread.Sleep(200);
+
+                IntPtr handle2 = GetHandleWindow("Список обучающихся");
+                WindowAPI.SendKeys(handle2, " ");
+
+            }
+            WindowAPI.SetActiveWindow(Process.GetCurrentProcess().MainWindowHandle);
+
         }
     }
 }
